98a93222d6f76faf291d2a8a4fb54ab11bdeb56e
pinganzhang <1596454155@qq.com>
2020-10-23 22:21:39 +0800

    to do delay

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@ const int gate = 1800;
  * @param business
  * @return
  */
-vector<int> init_packet(int kind, int business) {
+vector<int> initPacket(int kind, int business) {
     vector<int> packet(kind * business);
-    int max_size = 500;
+    int maxSize = 500;
     for (int i = 0; i < kind; i++) {
         for (int j = 0; j < business; j++) {
-            packet[i * business + j] = max_size;
+            packet[i * business + j] = maxSize;
         }
-        max_size -= 40;
+        maxSize -= 40;
     }
     return packet;
 }
@@ -30,18 +30,60 @@ vector<int> init_packet(int kind, int business) {
  * @param rate
  * @return
  */
-vector<double> get_standard_th(vector<int> pkt_size, int rate) {
-    vector<double> throughput(pkt_size.size());
-    for (int i = 0; i < pkt_size.size(); i++) {
-        throughput[i] = (pkt_size[i] * 8 * rate) / 1000;
+vector<double> getStandardThroughPut(vector<int> pktSize, int rate) {
+    vector<double> throughput(pktSize.size());
+    for (int i = 0; i < pktSize.size(); i++) {
+        throughput[i] = (pktSize[i] * 8 * rate) / 1000;
         double random = rand() % 500 + 500;
         throughput[i] -= 1 + random / 1000;
     }
     return throughput;
 }
 
-vector<double> solve_throughput(vector<double> throughput) {
+/**
+ * select which to decrease
+ * @param business
+ * @param neeToChange
+ * @return
+ */
+vector<int> initWhich(int business, int neeToChange) {
+    vector<int> flag(business, 0);
+    for (int i = 0; i < neeToChange; i++) {
+        int randomValue = rand() % business;
+        if (flag[randomValue] == 1) {
+            i--;
+        } else {
+            flag[randomValue] = 1;
+        }
+    }
+    return flag;
+}
 
+/**
+ * adjust throughput
+ * @param throughput
+ * @param business
+ * @return
+ */
+vector<double> solveThroughput(vector<double> throughput, int business) {
+    double sum = 0.000;
+    for (int i = 0; i < throughput.size(); i++) {
+        sum += throughput[i];
+    }
+    if (sum < gate) {
+        return throughput;
+    }
+    int randDecrease = rand() % 50;
+    double distance = sum + randDecrease - gate;
+    int needToFix = (rand() % business) + 3;
+    double average = distance / (double) needToFix;
+    vector<int> whichToChange = initWhich(business, needToFix);
+    for (int i = 0; i < whichToChange.size(); i++) {
+        if (whichToChange[i] == 1) {
+            throughput[i] -= average;
+        }
+    }
+    return throughput;
 }
 
 /**
@@ -49,10 +91,10 @@ vector<double> solve_throughput(vector<double> throughput) {
  * @param pkt_size
  * @return
  */
-vector<double> get_standard_delay(vector<int> pkt_size) {
-    vector<double> delay(pkt_size.size());
-    for (int i = 0; i < pkt_size.size(); i++) {
-        delay[i] = (pkt_size[i] + 104) * 8;
+vector<double> getStandardDelay(vector<int> pktSize) {
+    vector<double> delay(pktSize.size());
+    for (int i = 0; i < pktSize.size(); i++) {
+        delay[i] = (pktSize[i] + 104) * 8;
         delay[i] /= 1024;
         delay[i] /= BandWidth;
         delay[i] *= 1000;
@@ -62,29 +104,78 @@ vector<double> get_standard_delay(vector<int> pkt_size) {
     return delay;
 }
 
+/**
+ * get when to fix delay
+ * @param throughPut
+ * @param kind
+ * @param business
+ * @return
+ */
+int getTopValue(vector<double> throughPut, int kind, int business) {
+    double tmpTh = 0.000;
+    vector<int> index(kind);
+    for (int i = 0; i < kind; i++) {
+        index[i] = i * business;
+    }
+    for (int i = 0; i < business; i++) {
+        for (int j = 0; j < index.size(); j++) {
+            tmpTh += throughPut[index[j]];
+            index[j]++;
+        }
+        if (tmpTh > gate) {
+            return (i + 1);
+        }
+    }
+    return business;
+}
+
+vector<double> solveDelay(vector<double> delay, int business, int top) {
+    if (business < top) {
+        return delay;
+    }
+    int toFixBusiness = business - top + 1;
+    int start = 2;
+    while (toFixBusiness > 0) {
+        start *= 2;
+        toFixBusiness--;
+    }
+    return delay;
+}
+
 int main() {
-    int kind = 1;
+    int kind = 3;
     int business = 10;
-    int data_rate = 20; //  packets/s
-    int node_num = kind * business * 2;
+    int dataRate = 20; //  packets/s
+    int nodeNum = kind * business * 2;
 
     //  get packet size of each business
-    vector<int> packet_size = init_packet(kind, business);
-    for (int i = 0; i < packet_size.size(); i++) {
-        cout << packet_size[i] << " ";
+    vector<int> packetSize = initPacket(kind, business);
+    for (int i = 0; i < packetSize.size(); i++) {
+        cout << packetSize[i] << " ";
     }
     cout << endl;
 
     //  get standard delay of each business
-    vector<double> standard_delay = get_standard_delay(packet_size);
-    for (int i = 0; i < standard_delay.size(); i++) {
-        cout << standard_delay[i] << " ";
+    vector<double> standardDelay = getStandardDelay(packetSize);
+    for (int i = 0; i < standardDelay.size(); i++) {
+        cout << standardDelay[i] << " ";
     }
     cout << endl;
 
-    vector<double> standard_th = get_standard_th(packet_size, data_rate);
-    for (int i = 0; i < standard_th.size(); i++) {
-        cout << standard_th[i] << " ";
+    vector<double> standardThroughPut = getStandardThroughPut(packetSize, dataRate);
+    for (int i = 0; i < standardThroughPut.size(); i++) {
+        cout << standardThroughPut[i] << " ";
+    }
+    cout << endl;
+
+
+    int top = getTopValue(standardThroughPut, kind, business);
+    cout << top << endl;
+
+
+    standardThroughPut = solveThroughput(standardThroughPut, business);
+    for (int i = 0; i < standardThroughPut.size(); i++) {
+        cout << standardThroughPut[i] << " ";
     }
     cout << endl;
     return 0;
